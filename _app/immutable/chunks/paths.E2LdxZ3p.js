42bd7cde2943247dc47a0ee28e408e9afdff4c83
MrGoldenFox <mrgoldenfoxnear@gmail.com>
2024-02-28 21:29:58 +0100

    Updates

---
+++ _app/immutable/chunks/paths.E2LdxZ3p.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_94s3lr)==null?void 0:s.base)??"/boatbookings";var a;const e=((a=globalThis.__sveltekit_94s3lr)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.iZ7Xc5Hg.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C8kz93S3.js";export{t as start};
